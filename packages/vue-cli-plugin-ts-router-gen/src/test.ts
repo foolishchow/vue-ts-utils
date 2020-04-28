110780537185c4073d40bd51df68862ac93510c1
foolishchow <foolishchow@dingtalk.com>
2020-04-28 11:34:57 +0800

    update

--- packages/vue-cli-plugin-ts-router-gen/src/test.ts
+++ packages/vue-cli-plugin-ts-router-gen/src/test.ts
@@ -1,13 +1,8 @@
 import { Programer, RouteConfig } from "./watcher"
 
 process.title = "vue-cli-plugin-vuex-gen"
-let cwd = "/Users/zhoupeng/Desktop/Fireweed-code/fireweed-admin"
-try {
-  process.chdir(cwd);
-} catch (error) {
-  cwd = "/Users/hackintosh-01/Desktop/Fireweed-code/fireweed-admin"
-  process.chdir(cwd);
-}
+let cwd = ""
+process.chdir(cwd);
 
 let config: RouteConfig = {
   glob: ["src/pages/**/*.+(ts|tsx)"],
